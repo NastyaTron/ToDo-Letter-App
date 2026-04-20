d090016d3fcb497531885bf339dac20e95ef4f39
Anastasiia <151436238+NastyaTron@users.noreply.github.com>
2026-04-20 13:32:55 +0300

    add deletButton and toggle

--- src/main.js
+++ src/main.js
@@ -1,15 +1,37 @@
 const addButton = document.querySelector('.addButton');
 const title = document.querySelector('.title');
 const description = document.querySelector('.description');
-const list = document. querySelector('#All ul');
+const list = document.querySelector('#All ul');
 
+function createTodo(textTitle, textDescription) {
+    const todo = document.createElement('li');
+    todo.innerHTML = `<h3>${textTitle}</h3> <p>${textDescription}</p>`;
+   
+    todo.addEventListener('click', function(e){
+        e.preventDefault();
+        todo.classList.toggle('done');
+    })
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
 
+    deleteButton.addEventListener('click', function () {
+    todo.remove();
+})
+    todo.appendChild(deleteButton);
+    return todo;
+}
 
 addButton.addEventListener('click', function(e){
 e.preventDefault();
-const todo = document.createElement('li');
-todo.innerHTML = `<h3>${title.value}</h3> <p>${description.value}</p>`;
+
+if (title.value.trim() === '') {
+    alert('Enter title');
+    return
+}
+const todo = createTodo(title.value, description.value);
+
 list.appendChild(todo);
+
 title.value = '';
 description.value = '';
 })
